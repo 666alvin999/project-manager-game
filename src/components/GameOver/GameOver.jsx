d95dee7e475485dc5c432cb428cae5264fc6b2e8
Alyssa <alychrf62300@gmail.com>
2025-01-31 15:34:34 +0100

    mis a jour

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -8,11 +8,11 @@ const GameOver = ({ score }) => {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.8 }}
             transition={{ duration: 0.5 }}
-            className="absolute inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center"
+            className="absolute inset-0  bg-opacity-75 flex items-center justify-center h-[100vh] "
         >
-            <div className="text-center bg-white p-8 rounded shadow-lg">
+            <div className="text-center bg-white p-8 rounded shadow-lg mt-40">
                 <h1 className="text-3xl font-bold text-red-600 mb-4">Game Over!</h1>
-                <p className="text-xl mb-4">Votre score final : <span className="font-bold">{score}</span></p>
+                <p className="text-xl mb-4">Vous avez dépassez -20 <br></br> <span className="font-bold">{score}</span></p>
                 <button
                     onClick={() => window.location.reload()} // Recharger la page pour redémarrer
                     className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
